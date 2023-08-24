2a4f21d27ba6673594f4943c7aa9e76842074b16
PsCaveJack <breajack1@gmail.com>
2023-08-24 15:29:28 -0700

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
 import LoginPage from './login/LoginPage'
+import HomePage from './components/HomePage'
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './firebase.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+export const Firebase = initializeApp(firebaseConfig)
 
 function App() {
 
   return (
-      <div>
-        <LoginPage/>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element ={<HomePage/>}/>
+          <Route path = '/login' element ={<LoginPage/>}/>
+        </Routes>
+      </BrowserRouter>
+        
+      
   )
 }
 
