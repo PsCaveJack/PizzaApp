c69bc3c3c388b47eac046bc1698f948df6753e23
PsCaveJack <breajack1@gmail.com>
2023-08-24 14:03:16 -0700

    Initial commit

---
+++ src/login/LoginPage.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import './LoginPage.css'
+
+interface LoginCreds {
+    username: string,
+    password: string
+}
+
+const LoginPage: React.FC = () => {
+    const [credentials, setCredentials] = useState<LoginCreds>({
+        username: '',
+        password: '',
+      });
+    
+      const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setCredentials((prevCredentials) => ({
+          ...prevCredentials,
+          [name]: value,
+        }));
+      };
+    
+      const handleLogin = () => {
+        // Here you can implement your login logic using API calls or any authentication method
+        console.log('Logging in with:', credentials);
+      };
+    
+      return (
+        <div className="login-page">
+          <div className="login-card">
+            <h2>Login</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  value={credentials.username}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={credentials.password}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <button type="button" onClick={handleLogin}>
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      );
+}
+
+export default LoginPage
